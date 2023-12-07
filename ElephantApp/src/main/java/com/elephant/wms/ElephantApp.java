5f6fd5020a24d79f3f03043d45e526b1926b8f1b
sanxuan <java_researcher@163.com>
2023-12-07 22:53:34 +0800

    for more fast.

--- ElephantApp/src/main/java/com/elephant/wms/ElephantApp.java
+++ ElephantApp/src/main/java/com/elephant/wms/ElephantApp.java
@@ -1,16 +1,25 @@
 package com.elephant.wms;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@MapperScan({"com.elephant.wms.basic.infrastructure.mapper",
+@MapperScan({
+        "com.elephant.wms.basic.infrastructure.mapper",
         "com.elephant.wms.inventory.infrastructure.mapper",
         "com.elephant.wms.input.infrastructure.mapper"
 })
-@ImportResource(locations = {"classpath:camel.xml","classpath:/mapper/*.xml"})
+@ImportResource(locations = {
+        "classpath:camel.xml",
+        "classpath:/mapper/inventory_details.xml",
+        "classpath:/mapper/receive_order.xml"
+    }
+)
+@OpenAPIDefinition(info = @Info(title = "Elephant WMS API", version = "1.0", description = "Elephant WMS 接口相关信息"))
 public class ElephantApp {
 
     public static void main(String[] args) {
